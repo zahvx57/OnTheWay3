ca44eff094e619816b3790403c09d808df4d5e3d
zahvx57 <zahvx57@gmail.com>
2025-12-18 01:23:07 +0400

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,74 +3,117 @@ import Logo from "../assets/logo.png";
 import { UserSchemaValidation } from "../validations/userSchemaValidation";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { getUser, clearMessage } from "../features/UserSlice";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getUser } from "../features/UserSlice";
 import { useDispatch, useSelector } from "react-redux";
- 
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+
   const dispatch = useDispatch();
+  const user = useSelector((state) => state.users.user);
+  const isSuccess = useSelector((state) => state.users.isSuccess);
+  const isError = useSelector((state) => state.users.isError);
   const navigate = useNavigate();
-  const { user, isSuccess, isError, isLoading, message } = useSelector((s) => s.users);
- 
+
   const {
     register,
-    handleSubmit,
+    handleSubmit: submitForm,
     formState: { errors },
   } = useForm({ resolver: yupResolver(UserSchemaValidation) });
- 
-  const onSubmit = (data) => {
+
+  const validate = () => {
+    const data = {
+      email: email,
+      password: password,
+    };
     dispatch(getUser(data));
   };
- 
+
   useEffect(() => {
-    if (isSuccess && (user?._id || user?.email)) {
+    if (user && isSuccess) {
       navigate("/home");
-      dispatch(clearMessage());
     }
-  }, [isSuccess, user, navigate, dispatch]);
- 
+    if (isError) {
+      navigate("/");
+    }
+  }, [user, isSuccess, isError, navigate]);
+
   return (
-<div>
-<Container fluid>
-<Row className="div-row">
-<Col md="6" className="div-col">
-<form className="div-form" onSubmit={handleSubmit(onSubmit)}>
-<div>
-<img alt="Logo" className="img-fluid rounded mx-auto d-block" src={Logo} />
-</div>
- 
+    <div>
+      <Container fluid>
+        <Row className="div-row">
+          <Col md="6" className="div-col">
+            <form className="div-form">
+              <div>
+                <img
+                  alt="Logo"
+                  className="img-fluid rounded mx-auto d-block"
+                  src={Logo}
+                />
+              </div>
+
               <FormGroup>
-<Label>Email</Label>
-<Input {...register("email")} placeholder="Please Enter your Email..." type="email" />
-<p style={{ color: "red" }}>{errors.email?.message}</p>
-</FormGroup>
- 
+                <Label>Email</Label>
+                <input
+                  {...register("email", {
+                    value: email,
+                    onChange: (e) => setEmail(e.target.value),
+                  })}
+                  placeholder="Please Enter your Email here..."
+                  type="email"
+                  className="form-control"
+                />
+                <p style={{ color: "red" }}>{errors.email?.message}</p>
+              </FormGroup>
+
               <FormGroup>
-<Label>Password</Label>
-<Input {...register("password")} placeholder="Please Enter your Password..." type="password" />
-<p style={{ color: "red" }}>{errors.password?.message}</p>
-</FormGroup>
- 
-              {isError && <p style={{ color: "red" }}>{message}</p>}
- 
+                <Label>Password</Label>
+                <input
+                  {...register("password", {
+                    value: password,
+                    onChange: (e) => setPassword(e.target.value),
+                  })}
+                  placeholder="Please Enter your Password here..."
+                  type="password"
+                  className="form-control"
+                />
+                <p style={{ color: "red" }}>{errors.password?.message}</p>
+              </FormGroup>
+
               <FormGroup>
-<Button type="submit" className="form-control" color="dark" disabled={isLoading}>
-                  {isLoading ? "Signing in..." : "Sign In"}
-</Button>
-</FormGroup>
- 
+                <Input type="checkbox" /> <Label>Remmber Me</Label>
+              </FormGroup>
+
+              <FormGroup>
+                <Button
+                  type="button"  // ✅ added to prevent form submit issues
+                  onClick={submitForm(validate)}
+                  className="form-control"
+                  color="dark"
+                >
+                  Sign In
+                </Button>
+              </FormGroup>
+
               <FormGroup className="text-center">
-<Label>
+                <Label>Forget password</Label>
+              </FormGroup>
+
+              <FormGroup className="text-center">
+                <Label>
                   No Account? <Link to="/register">Sign Up Now...</Link>
-</Label>
-</FormGroup>
-</form>
-</Col>
-</Row>
-</Container>
-</div>
+                </Label>
+              </FormGroup>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
- 
+
 export default Login;
