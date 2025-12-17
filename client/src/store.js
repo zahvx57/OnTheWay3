567a6011b4a563f2ba205db56d3c84df9ed2d7de
zahvx57 <zahvx57@gmail.com>
2025-12-18 02:04:12 +0400

    Update store.js

--- client/src/store.js
+++ client/src/store.js
@@ -2,13 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./features/UserSlice";
 import favoriteReducer from "./features/FavoriteSlice";
 import selectedDelegateReducer from "./features/SelectedDelegateSlice";
-import themeReducer from "./features/ThemeSlice"; // ✅ أضف هذا
+import themeReducer from "./features/ThemeSlice";
+import placesReducer from "./features/PlacesSlice";
 
 export const store = configureStore({
   reducer: {
     users: userReducer,
     favorites: favoriteReducer,
     selectedDelegate: selectedDelegateReducer,
-    theme: themeReducer, // ✅ أضف هذا
+    theme: themeReducer,
+    places: placesReducer,
   },
 });
