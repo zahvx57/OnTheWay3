b81d79f62c5f958afb8ec38a9e46776336abf4f2
zahvx57 <zahvx57@gmail.com>
2025-12-18 02:20:23 +0400

    Update Register.js

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -6,96 +6,108 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { addUser } from "../features/UserSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
- 
+
 const Register = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const message = useSelector((state) => state.users.message);
- 
+
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({ resolver: yupResolver(UserRegisterSchemaValidation) });
- 
+  } = useForm({
+    resolver: yupResolver(UserRegisterSchemaValidation),
+    mode: "onSubmit",
+  });
+
+  // ✅ Reactstrap + RHF fix
+  const { ref: unameRef, ...unameReg } = register("uname");
+  const { ref: picRef, ...picReg } = register("pic");
+  const { ref: emailRef, ...emailReg } = register("email");
+  const { ref: passRef, ...passReg } = register("password");
+
   const onSubmit = (data) => {
-    // data = { uname, pic, email, password }
     const payload = {
       uname: data.uname,
       email: data.email,
       password: data.password,
       profilepic: data.pic, // ✅ نفس اسم السيرفر
     };
- 
+
     dispatch(addUser(payload));
     navigate("/");
   };
- 
+
   return (
-<div>
-<Container fluid>
-<Row className="div-row">
-<Col md="6" className="div-col">
-<form className="div-form" onSubmit={handleSubmit(onSubmit)}>
-<div>
-<img alt="Logo" className="img-fluid rounded mx-auto d-block" src={Logo} />
-</div>
- 
+    <div>
+      <Container fluid>
+        <Row className="div-row">
+          <Col md="6" className="div-col">
+            <form className="div-form" onSubmit={handleSubmit(onSubmit)}>
+              <div>
+                <img alt="Logo" className="img-fluid rounded mx-auto d-block" src={Logo} />
+              </div>
+
               <FormGroup>
-<Label>UserName:</Label>
-<Input
-                  {...register("uname")}
+                <Label>UserName:</Label>
+                <Input
+                  innerRef={unameRef}
+                  {...unameReg}
                   placeholder="Please Enter your username here..."
                   type="text"
                 />
-<p style={{ color: "red" }}>{errors.uname?.message}</p>
-</FormGroup>
- 
+                {errors.uname && <p style={{ color: "red" }}>{errors.uname.message}</p>}
+              </FormGroup>
+
               <FormGroup>
-<Label>Profile Picture:</Label>
-<Input
-                  {...register("pic")}
+                <Label>Profile Picture:</Label>
+                <Input
+                  innerRef={picRef}
+                  {...picReg}
                   placeholder="Please Enter your Profile picture here..."
                   type="text"
                 />
-<p style={{ color: "red" }}>{errors.pic?.message}</p>
-</FormGroup>
- 
+                {errors.pic && <p style={{ color: "red" }}>{errors.pic.message}</p>}
+              </FormGroup>
+
               <FormGroup>
-<Label>Email:</Label>
-<Input
-                  {...register("email")}
+                <Label>Email:</Label>
+                <Input
+                  innerRef={emailRef}
+                  {...emailReg}
                   placeholder="Please Enter your Email here..."
                   type="email"
                 />
-<p style={{ color: "red" }}>{errors.email?.message}</p>
-</FormGroup>
- 
+                {errors.email && <p style={{ color: "red" }}>{errors.email.message}</p>}
+              </FormGroup>
+
               <FormGroup>
-<Label>Password:</Label>
-<Input
-                  {...register("password")}
+                <Label>Password:</Label>
+                <Input
+                  innerRef={passRef}
+                  {...passReg}
                   placeholder="Please Enter your Password here..."
                   type="password"
                 />
-<p style={{ color: "red" }}>{errors.password?.message}</p>
-</FormGroup>
- 
+                {errors.password && <p style={{ color: "red" }}>{errors.password.message}</p>}
+              </FormGroup>
+
               <FormGroup>
-<Button type="submit" className="form-control" color="dark">
+                <Button type="submit" className="form-control" color="dark">
                   Sign Up
-</Button>
-</FormGroup>
- 
+                </Button>
+              </FormGroup>
+
               <FormGroup>
-<p>{message}</p>
-</FormGroup>
-</form>
-</Col>
-</Row>
-</Container>
-</div>
+                <p>{message}</p>
+              </FormGroup>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
- 
+
 export default Register;
