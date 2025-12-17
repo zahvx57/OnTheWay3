ddfaaac7c7fedaeb4dd0cefcb5d66225c0e8d63b
zahvx57 <zahvx57@gmail.com>
2025-12-18 01:48:52 +0400

    Update Register.js

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,57 +1,34 @@
 import { Container, Row, Col, FormGroup, Label, Button, Input } from "reactstrap";
-
 import Logo from "../assets/logo.png";
-
 import { UserRegisterSchemaValidation } from "../validations/UserRegisterSchemaValidation";
-
 import { useForm } from "react-hook-form";
-
 import { yupResolver } from "@hookform/resolvers/yup";
-
 import { addUser } from "../features/UserSlice";
-
 import { useDispatch, useSelector } from "react-redux";
-
 import { useNavigate } from "react-router-dom";
  
 const Register = () => {
-
   const dispatch = useDispatch();
-
   const navigate = useNavigate();
-
   const message = useSelector((state) => state.users.message);
  
   const {
-
     register,
-
     handleSubmit,
-
     formState: { errors },
-
   } = useForm({ resolver: yupResolver(UserRegisterSchemaValidation) });
  
   const onSubmit = (data) => {
-
     // data = { uname, pic, email, password }
-
     const payload = {
-
       uname: data.uname,
-
       email: data.email,
-
       password: data.password,
-
       profilepic: data.pic, // ✅ نفس اسم السيرفر
-
     };
  
     dispatch(addUser(payload));
-
     navigate("/");
-
   };
  
   return (
@@ -67,13 +44,9 @@ const Register = () => {
               <FormGroup>
 <Label>UserName:</Label>
 <Input
-
                   {...register("uname")}
-
                   placeholder="Please Enter your username here..."
-
                   type="text"
-
                 />
 <p style={{ color: "red" }}>{errors.uname?.message}</p>
 </FormGroup>
@@ -81,13 +54,9 @@ const Register = () => {
               <FormGroup>
 <Label>Profile Picture:</Label>
 <Input
-
                   {...register("pic")}
-
                   placeholder="Please Enter your Profile picture here..."
-
                   type="text"
-
                 />
 <p style={{ color: "red" }}>{errors.pic?.message}</p>
 </FormGroup>
@@ -95,13 +64,9 @@ const Register = () => {
               <FormGroup>
 <Label>Email:</Label>
 <Input
-
                   {...register("email")}
-
                   placeholder="Please Enter your Email here..."
-
                   type="email"
-
                 />
 <p style={{ color: "red" }}>{errors.email?.message}</p>
 </FormGroup>
@@ -109,20 +74,15 @@ const Register = () => {
               <FormGroup>
 <Label>Password:</Label>
 <Input
-
                   {...register("password")}
-
                   placeholder="Please Enter your Password here..."
-
                   type="password"
-
                 />
 <p style={{ color: "red" }}>{errors.password?.message}</p>
 </FormGroup>
  
               <FormGroup>
 <Button type="submit" className="form-control" color="dark">
-
                   Sign Up
 </Button>
 </FormGroup>
@@ -135,11 +95,7 @@ const Register = () => {
 </Row>
 </Container>
 </div>
-
   );
-
 };
  
 export default Register;
-
- 
