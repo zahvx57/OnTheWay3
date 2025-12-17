8fac5f46949a52ba1871a6cdc40ff3b79486a044
zahvx57 <zahvx57@gmail.com>
2025-12-18 01:03:05 +0400

    Update SelectedDelegateSlice.js

--- client/src/features/SelectedDelegateSlice.js
+++ client/src/features/SelectedDelegateSlice.js
@@ -1,21 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+ 
 const SelectedDelegateSlice = createSlice({
+
   name: "selectedDelegate",
+
   initialState: {
+
     delegate: null,
+
   },
+
   reducers: {
+
     setSelectedDelegate: (state, action) => {
+
       state.delegate = action.payload;
+
     },
+
     clearSelectedDelegate: (state) => {
+
       state.delegate = null;
+
     },
+
   },
+
 });
-
+ 
 export const { setSelectedDelegate, clearSelectedDelegate } =
-  SelectedDelegateSlice.actions;
 
+  SelectedDelegateSlice.actions;
+ 
 export default SelectedDelegateSlice.reducer;
+ 
